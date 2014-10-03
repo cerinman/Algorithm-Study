5dbc59bab415889f0739ac7295b2d3f2d4546dae
tommy.dugger78@gmail.com <tommy.dugger78@gmail.com>
2014-10-02 20:37:43 -0600

    Adds new quick-sort directory and quick-sort.rb file.  Adds resource and note comments to the quick-sort.rb file

---
+++ quick-sort/quick-sort.rb
@@ -0,0 +1,14 @@
+#Resources:
+
+#https://www.youtube.com/watch?v=aQiWF4E8flQ
+
+#Quick sort is O(n 2) at worst but for some datasets it can
+#actually behave more like O(n log n).  This is typically
+#because of the size of the dataset.  The smaller the dataset
+#the better Quick sort will perform.  It is also worth noting
+#that quick sort does much better if the first pivot chosen
+#for comparison is some where in the median of the array.
+
+def quick_sort(array, from = 0, to = nil)
+
+end
