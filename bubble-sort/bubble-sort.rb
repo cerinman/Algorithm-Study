763742960e440c9f29551450bf2184a71646b380
tommy.dugger78@gmail.com <tommy.dugger78@gmail.com>
2014-10-02 21:42:13 -0600

    Adds new bubble-sort director and bubble-sort.rb file.  Adds resource and note comments to bubble-sort.rb file

---
+++ bubble-sort/bubble-sort.rb
@@ -0,0 +1,12 @@
+# Resources:
+
+#https://www.youtube.com/watch?v=8Kp-8OGwphY
+
+#Bubble sort is O(n 2)
+
+#Bubble sort works by comparing each item in a set to its
+#immediate right neighbor and swapping them if they are not
+#in order.  This happens continually until no swaps have
+#been made on a single pass and the dataset is completely
+#sorted.
+
