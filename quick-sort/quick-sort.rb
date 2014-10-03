69aab36b33735e2be9341f4a62c7f22007b79594
tommy.dugger78@gmail.com <tommy.dugger78@gmail.com>
2014-10-02 20:41:31 -0600

    Adds guard checks for sorting being complete and initial set of to variable to start

--- quick-sort/quick-sort.rb
+++ quick-sort/quick-sort.rb
@@ -8,7 +8,21 @@
 #the better Quick sort will perform.  It is also worth noting
 #that quick sort does much better if the first pivot chosen
 #for comparison is some where in the median of the array.
+#
+#Quick sort works in place
 
 def quick_sort(array, from = 0, to = nil)
 
+  #By default we start by sorting the entire
+  #array.
+  if to == nil
+    to = array.count - 1
+  end
+
+  #Because this sort is in place, we don't need
+  #to return anything.  This is the guard check
+  #for sorting being complete.
+  if from >= to
+    return
+  end
 end
