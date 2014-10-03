710b8f0fbc8c8b21eb2a284351e030dd25f8fdbd
tommy.dugger78@gmail.com <tommy.dugger78@gmail.com>
2014-10-02 19:25:10 -0600

    Adds notes and resource comments.  Adds empty merge-sort method

---
+++ merge-sort/merge-sort.rb
@@ -0,0 +1,14 @@
+#Resources:
+#
+#https://www.youtube.com/watch?v=EeQ8pwjQxTM
+
+#Merge sort is O(n log n)
+
+#Merge sort does take more space because of the extra arrays or lists
+#that it uses to sort but generally speaking the space/time trade off
+#is worth it as n log n is about as fast as you can git with sorting
+#algorithms
+
+def merge_sort(array)
+
+end
